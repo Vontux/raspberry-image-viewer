3b2f864439dfebab56aba8238a3bca0e642aa4a9
Vontux <mbeaven001@gmail.com>
2017-06-14 15:54:48 -0700

    Create showimages.sh
    initial changes to display images directly from thumbdrive and show the please insert images request to the user via the 'nopics.png' image that will be present in the images directory which will function as the repository for images that will remain on the pi at all times.

--- showimages.sh
+++ showimages.sh
@@ -9,47 +9,14 @@ sudo mount -t vfat -o rw,users /dev/sda1 $USBDIR
 # if usb folder is not empty (a usb with files on it is mounted)
 if [ "$(ls -A $USBDIR)" ]
 then
-	echo "USB found, copying new images"
-
-	# remove all old files
-	rm $IMGDIR/*.jpg
-	rm $IMGDIR/*.png
-
-	# copy images
-	echo "copy *.jpg and *.png from USB"
-	cp $USBDIR/*.jpg $IMGDIR/
-	cp $USBDIR/*.png $IMGDIR/
+	echo "USB found, displaying images"
+	fbi -a -u -t 12 --blend 200 --readahead --noverbose -T 1 $USBDIR/* -d /dev/fb0
+	
 else
-	echo "USB NOT found, using current images"
+	echo "USB with images NOT found, requesting images"
+	#load image to request user input drive with images to load and reboot
+	fbi -a -u -t 12 --blend 200 --readahead --noverbose -T 1 $IMGDIR/nopics.png -d /dev/fb0
+	echo "unmount usb"
+	umount $USBDIR
+	#/usr/bin/fbi -a -noverbose -t 5  $IMGDIR/nopics.png
 fi
-
-echo "unmount usb"
-umount $USBDIR
-
-sleep 2
-
-filesstring=""
-
-# check for files in image dir
-jpgfiles=`ls -1 $IMGDIR/*.jpg 2>/dev/null | wc -l`
-pngfiles=`ls -1 $IMGDIR/*.png 2>/dev/null | wc -l`
-
-if [ $jpgfiles = 0 ] ; then
-	if [ $pngfiles = 0 ] ; then
-		echo "no images files found, aborting"
-		exit 0
-	fi
-fi 
-
-if [ $jpgfiles != 0 ]; then 
-	echo "found jpg"
-	filesstring="$filesstring $IMGDIR/*.jpg"
-fi
-
-if [ $pngfiles != 0 ]; then 
-	echo "found png"
-	filesstring="$filesstring $IMGDIR/*.png"
-fi
-
-echo "showing 5 second slideshow with $filesstring"
-/usr/bin/fbi -a -noverbose -t 5  $filesstring
