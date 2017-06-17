1868b00959935c1d4404a304576c2ca1d73d5142
Vontux <mbeaven001@gmail.com>
2017-06-16 21:48:06 -0700

    Add files via upload
    script to manage poweroff and reboot via GPIO button presses, GPIO 13 & GPIO 21

---
+++ poweroff.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#tweaked a script from an adafruit tutorial
+#https://learn.adafruit.com/playing-sounds-and-using-buttons-with-raspberry-pi?view=all
+import os
+from time import sleep
+ 
+import RPi.GPIO as GPIO
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+ 
+while True:
+    if (GPIO.input(13) == False):
+        os.system('sudo shutdown reboot')
+ 
+    if (GPIO.input(21) == False):
+        os.system('sudo shutdown -h now')
+ 
+ 
+    sleep(0.1);
